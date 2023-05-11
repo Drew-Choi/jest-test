fe3073ddd7fcf624a5742e5ddb78d70a69ea3ac1
Drew <drc920630@gmail.com>
2023-05-11 15:56:53 +0900

    TS테스트 중

--- __test__/suite.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('../test_sum');
-
-test("1 ㄷ더하기 2는 3이 나와야겠지?!", () => {
-  expect(sum(1, 2)).toBe(3);
-})
---
+++ test.ts
@@ -0,0 +1,7 @@
+export const sum = (x: number, y: number): number => {
+  return x + y;
+}
+
+test("TS 테스트 케이스 작성하기!", ()=> {
+  expect(sum(1,2)).toBe(3);
+})
